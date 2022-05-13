026105e72d5212020b301cfdf2baab5c20828b68
iniline <1004dlagywls@naver.com>
2022-05-13 17:36:56 +0900

    BufferedReader, InputStreamReader, StringBuilder를 이용한 A+B 구하는 식 null이면 종료 NoSuchElementException 떠서 처리 함... 뭐지

--- src/test9.java
+++ src/test9.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
-public class Main {
+public class test9 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
