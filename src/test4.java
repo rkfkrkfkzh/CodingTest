e73bd529c8f8428fd5baaf22b85b5bb9e9d890fb
iniline <1004dlagywls@naver.com>
2022-05-13 15:14:55 +0900

    BufferedReader, InputStreamReader, StringBuilder를 이용한 A+B 아름답게 계산하기

--- src/test4.java
+++ src/test4.java
@@ -3,19 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class test4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int a;
-        int b;
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-             a = Integer.parseInt(st.nextToken());
-             b = Integer.parseInt(st.nextToken());
-            sb.append("Case #").append(i).append(": ").append(a+ " + "+b+" = "+ (a+b)).append("\n");
+            sb.append("Case #").append(i).append(": ").append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+
         }
         br.close();
         System.out.println(sb);
--- src/test5.java
+++ src/test5.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class test5 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -13,9 +13,10 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-             a = Integer.parseInt(st.nextToken());
-             b = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
             sb.append("Case #").append(i).append(": ").append(a+ " + "+b+" = "+ (a+b)).append("\n");
+
         }
         br.close();
         System.out.println(sb);
