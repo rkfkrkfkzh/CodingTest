095582f1e75b0640d9db37f4bd35113513e3c503
iniline <1004dlagywls@naver.com>
2022-05-19 10:21:36 +0900

    1차원 배열 최대값 구하기 BufferedReader 사용 배열없이도 해보았다

--- src/test11.java
+++ src/test11.java
@@ -6,25 +6,33 @@ public class test11 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[9];
+//        int[] arr = new int[9];
 
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+//        for (int i = 0; i < 9; i++) {
+//            arr[i] = Integer.parseInt(br.readLine());
+//        }
         int max = 0;
         int index = 0;
 
-        int count = 0;
-
-//      for( A : B)오른쪽의 리스트의 인덱스를 순차적으로 접근하여 왼쪽에 있는 변수에 저장하겠다는 것
-        for (int value : arr) {
-            count++;
-            if (value > max) {
-                max = value;
-                index = count;
+//        int count = 0;
+        for (int i = 0; i < 9; i++) {
+            int val = Integer.parseInt(br.readLine());
+            if (val > max) {
+                max = val;
+                index = i + 1;
             }
         }
-        System.out.println(index);
+
+
+//      for( A : B)오른쪽의 리스트의 인덱스를 순차적으로 접근하여 왼쪽에 있는 변수에 저장하겠다는 것
+//        for (int value : arr) {
+//            count++;
+//            if (value > max) {
+//                max = value;
+//                index = count;
+//            }
+//        }
         System.out.println(max);
+        System.out.println(index);
     }
 }
