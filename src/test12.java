19cb6e5686466ca70fb34b31bcf3f93cb47912db
iniline <1004dlagywls@naver.com>
2022-05-20 17:15:22 +0900

    Byte값 하나만 read하여 아스키코드값 저장

--- src/test12.java
+++ src/test12.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
-public class Main  {
-    public static void main(String[] args)throws IOException {
+public class test12 {
+    public static void main(String[] args) throws IOException {
         int a = System.in.read(); // System.in 은 byte 값으로 문자 한 개만 읽으면서 해당 문자에 대응되는 아스키코드 값을 저장
         System.out.println(a);
     }
