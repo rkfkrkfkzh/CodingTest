f857049f0222427ffec825fd3298b4c096ea0841
iniline <1004dlagywls@naver.com>
2022-05-23 14:59:58 +0900

    숫자의 개수

--- src/test13.java
+++ src/test13.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class test13 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[10];    // 0부터 9까지 체크할 길이 10의 int 배열을 생성
@@ -14,7 +14,7 @@ public class Main {
             arr[str.charAt(i) - 48]++;
 //      ASCII Code 문자를 10진으로 변환해서 -48해준다
         }
-        for (int v : arr) { // arr 값 v로 계속 넘겨줌 arr 다될떄까지
+        for (int v : arr) { // arr 값 v로 계속 넘겨줌 arr 다 될 때까지
             System.out.println(v);
         }
     }
