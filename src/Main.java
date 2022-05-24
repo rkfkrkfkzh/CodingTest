e1baaa555c2e173d14735218dce6f240e21c461f
iniline <1004dlagywls@naver.com>
2022-05-24 09:46:45 +0900

    System.in, InputStream, InputStreamReader, BufferedReader 재정의

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,12 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<Integer> h = new HashSet<Integer>();    //  HashSet<Integer> 으로 타입을 Integer로 선언
-//        HashSet 은 자바 Collection 중 Set 의 파생클래스
-//        중복되는 원소를 넣을 경우 하나만 저장한다. 즉, 중복원소를 허용하지 않는다
-//        HashSet 은 순서 개념이 없다. 따라서 Collections.sort() 메소드를 사용할 수 없다.
-//        만약 정렬을 하고 싶다면 리스트로 변환 후 정렬해야한다.
 
+        HashSet<Integer> h = new HashSet<Integer>();
 
         for (int i = 0; i < 10; i++) {
-            h.add(Integer.parseInt(br.readLine())%42);
-//            HashSet.add() 메소드는 HashSet에 저장하는 메소드
-//            이 메소드에서 값을 넣을 때 만약 중복되는 값이 없으면 HashSet 에 저장되면서 True 를 반환하고,
-//            만약 중복되어 저장되지 않으면 False 를 반환
+            h.add(Integer.parseInt(br.readLine()) % 42);
         }
         System.out.println(h.size());
-//        HashSet.size() 는 HashSet 의 크기(= 저장되어 있는 원소의 개수)를 반환
-//        HashSet 에 저장하면서 들어간 원소의 개수가 HashSet 의 사이즈
-
     }
 }
--- src/test.java
+++ src/test.java
@@ -9,6 +9,15 @@ public class test {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        입력을 받는 클래스인 InputStreamReader(System.in)을 객체로 생성
 //        해당 객체에서 각 입력을 버퍼에 저장하여 string에 저장할 수 있는 BufferedReader 클래스의 객체를 생성
+
+//        InputStream 은 Byte 단위로 data를 처리한다.
+//        System.in 의 type도 InputStream 이다.
+
+//        InputStreamReader 은 문자(character) 단위로 데이터를 처리할 수 있도록 돕는다.
+//        InputStream 의 데이터를 문자로 변환하는 중개 역할을 한다.
+
+//        BufferedReader 은 스트림에 버퍼를 두어 문자를 버퍼에 일정 정도 저장해둔 뒤 한 번에 보낸다.
+
         int A = Integer.parseInt(br.readLine());
         int B = Integer.parseInt(br.readLine());
 //        BufferedReader의 method인 readLine()을 이용
