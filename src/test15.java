e179d007d5694dba9bf11994f8799fe56635b888
iniline <1004dlagywls@naver.com>
2022-05-31 15:02:11 +0900

    숫자 합 표시 getBytes와 BufferedReader

--- src/test15.java
+++ src/test15.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class test15 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();  // N 은 쓸모가 없으므로 입력만 받는다.
