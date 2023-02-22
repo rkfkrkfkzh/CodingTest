1999584b270ab9f55e09c911d96a9004f52fad92
LimHyoJin <1004dlagywls@naver.com>
2023-02-22 16:57:18 +0900

    split을 이용하요 부분합 최솟값 구하기

---
+++ src/Greedy5.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Greedy5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] arr = br.readLine().split("-");
+
+        for (int i = 0; i < arr.length; i++) {
+            String [] addarr = arr[i].split("\\+");
+
+
+        }
+    }
+}
