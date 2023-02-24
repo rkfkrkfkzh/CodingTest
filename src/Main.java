01baa0d264104f1face361ed4ceee5903e9b9718
LimHyoJin <1004dlagywls@naver.com>
2023-02-24 16:49:10 +0900

    차례대로 로프를 꺼내면서 최대 중량을 계산한다. 예로 30 60 90 이면 이떄 들수 있는 최대는 90이다. 30 60 90 10 20 이면 최대는 50이다. 나는 단편적으로 최솟값에서 그냥 곱했는데 잘 읽어보면 차례대로 로프를 꺼낸다 하였고 모든 로프를 사용할 필요는 없단다

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int price = Integer.parseInt(br.readLine());
-        int[] changes = {500, 100, 50, 10, 5, 1};
-
-        int change_coin = 1000 - price;
-        int count = 0;
-        for (int i = 0; i < changes.length; i++) {
-            count += change_coin / changes[i];
-            change_coin = change_coin % changes[i];
+        //첫 번째 줄에 입력된 정수 N은 로프의 개수를 의미합니다.
+        int N = Integer.parseInt(br.readLine());
+//두 번째 줄부터 N개의 줄에 걸쳐 입력된 정수는 각 로프의 중량을 의미합니다.
+        int[] rope = new int[N];
+        for (int i = 0; i < N; i++) {
+            rope[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(count);
+        //Arrays.sort() 메서드를 사용하여 입력된 로프의 중량을 오름차순으로 정렬합니다.
+        Arrays.sort(rope);
+        int maxWeight = 0;
+        //가장 무거운 로프부터 순서대로 선택하면서 선택한 로프의 중량과 선택한 로프의 개수를 곱한 값을 계산합니다.
+        for (int i = N - 1; i >= 0; i--) {
+            // 선택한 로프의 중량과 선택한 로프의 개수를 곱한 값을 계산합니다.
+            int weight = rope[i] * (N - i);
+            if (maxWeight < weight) {
+                maxWeight = weight;
+            }
+        }
+        System.out.println(maxWeight);
     }
 }
