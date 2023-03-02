90f27296f8401685352ac2c7efcc0807b9a0ec87
LimHyoJin <1004dlagywls@naver.com>
2023-03-02 17:00:11 +0900

    BFS로 풀어보는중

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
+import java.awt.desktop.ScreenSleepEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        StringTokenizer str0 = new StringTokenizer(S, "0");
-        StringTokenizer str1 = new StringTokenizer(S, "1");
-        System.out.println(Math.min(str1.countTokens(), str0.countTokens()));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        br.close();
+    }
+    public static int bfs(int A, int B){
+        Queue<Integer> = new LinkedList<>();
+
     }
 }
