36ef4d42d0a34be06e5bf434b0e4b1ba8579a6f3
LimHyoJin <1004dlagywls@naver.com>
2023-03-02 11:16:35 +0900

    백준 4796 Greedy문제 입니다. 수학적으로 그냥 풀어봅니다.

--- src/Greedy14.java
+++ src/Greedy14.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Greedy14 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int i = 1;
