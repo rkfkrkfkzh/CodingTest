9ebe0e35bf65e2cf79b7a23e833380a5409f6be3
LimHyoJin <1004dlagywls@naver.com>
2023-03-03 11:04:51 +0900

    A->B BFS함수로 큐를 사용하여 탐색, cnt변수를 사용 변환 연산횟수를 계산한다. BufferedReader가 빠르고 메모리도 적게 차지 하더라

--- src/Greedy16.java
+++ src/Greedy16.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Greedy16 {
     static int INF = 987654321;
 
     public static void main(String[] args) throws IOException {
